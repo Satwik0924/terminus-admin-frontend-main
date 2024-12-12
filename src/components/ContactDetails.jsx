06b9008fde1e2187a768340a9457e8bd72c466bc
AnirudhMemani <nemanianirudh@gmail.com>
2024-12-12 06:31:39 +0530

    some more fixes

--- src/components/ContactDetails.jsx
+++ src/components/ContactDetails.jsx
@@ -2,37 +2,37 @@ import React from "react";
 
 const ContactDetails = () => {
   return (
-    <section className="max-w-4xl mx-11 px-11">
+    <section className="max-w-4xl mx-11 px-11 pt-16">
       <div className="flex flex-col space-y-4">
         {/* Contact Section */}
-        <div className="flex items-center">
-          <label className="font-bold text-lg text-black w-32">Contact</label>
-          <p className="text-black">+91 95548 34567</p>
+        <div className="flex items-start text-xl text-black">
+          <label className="font-bold w-32">Contact</label>
+          <p>+91 95548 34567</p>
         </div>
 
         {/* Address Section */}
-        <div className="flex items-center">
-          <label className="font-bold text-lg text-black w-32">Address</label>
-          <p className="text-black">
+        <div className="flex items-start text-black text-xl">
+          <label className="font-bold w-32">Address</label>
+          <p>
             25th Floor, ONE WEST Building, Sy. No.88/AA, 88/E,<br></br> Nanakramguda, Hyderabad, Telangana-500008
           </p>
         </div>
 
         {/* Email Section */}
-        <div className="flex items-center">
-          <label className="font-bold text-lg text-black w-32">Email</label>
-          <p className="text-black">info@terminus-global.com</p>
+        <div className="flex items-center text-xl text-black">
+          <label className="font-bold w-32">Email</label>
+          <p className="text-foreground">info@terminus-global.com</p>
         </div>
 
         {/* Message Section */}
-        <div className="flex items-center ">
-          <label className="font-bold text-lg text-black w-32"></label>
-          <p className="text-black">
-            We are always on the look<br></br>out for talent to add to our growing, dynamic team. Please click<br></br>{" "}
+        <div className="flex items-center">
+          <label className="font-bold text-black w-32"></label>
+          <p className="text-black text-xl space-y-6">
+            We are always on the look-<br></br>out for talent to add to our growing, dynamic team. Please click<br></br>{" "}
             on the link below to view and apply for open positions.<br></br>
             <a
               href="https://careers.terminus-global.com"
-              className="text-blue-600 hover:underline"
+              className="text-foreground font-bold"
               target="_blank"
               rel="noopener noreferrer"
             >
