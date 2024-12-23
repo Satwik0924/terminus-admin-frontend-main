2d368bc2f89fbe799275c46ec5301c3a5b82c073
kairvee <imt_2023037@iiitm.ac.in>
2024-12-23 12:37:03 +0530

    reslving

--- src/components/Awards.jsx
+++ src/components/Awards.jsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
+import { cn } from "@/lib/utils";
 import axios from "axios";
+import { useEffect, useState } from "react";
 
-const AwardsComponent = () => {
+const AwardsComponent = ({ className }) => {
   const [awardsData, setAwardsData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [scrolled, setScrolled] = useState(false);
@@ -38,26 +39,23 @@ const AwardsComponent = () => {
   }, []);
 
   return (
-    <div id="awards-section" className="px-5 py-12 max-w-screen-xl mx-auto">
+    <div id="awards-section" className={cn("mt-5 mb-12", className)}>
       {loading ? (
-        <p className="text-center">Loading awards data...</p>
+        <p className="text-center">Fetching our awards...</p>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {awardsData.map((award) => (
-            <div key={award.id} className="relative group overflow-hidden bg-white">
-              {/* Image container with hover effect */}
-              <div className="relative w-full h-64 overflow-hidden">
-                <img src={award.image} alt={award.title} className="w-full h-full object-cover" />
-                <div className="absolute inset-0 flex justify-top items-top bg-white/50 bg-opacity-70 opacity-0 group-hover:opacity-100 transition duration-500 [word-spacing:4px]">
-                  <div className="p-4">
-                    <p className="text-black text-left text-lg font-semibold">View More</p>
-                  </div>
-                </div>
+            <div key={award.id} className="overflow-hidden bg-white ">
+              <div className="w-full h-[400px] overflow-hidden">
+                <img
+                  src={award.image}
+                  alt={award.title}
+                  className="w-full h-full object-cover transition-transform duration-300 hover:blur-sm"
+                />
               </div>
-              {/* Text content */}
-              <div className="p-0">
-                <h3 className="text-3xl tracking-tighter text-black mb-2">{award.title}</h3>
-                <p className="text-lg text-black">{award.description}</p>
+              <div className="mt-4">
+                <h3 className="text-3xl tracking-tighter text-black line-clamp-1">{award.title}</h3>
+                <p className="text-lg text-black tracking-tighter !leading-tight line-clamp-2">{award.description}</p>
               </div>
             </div>
           ))}
