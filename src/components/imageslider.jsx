bf24429292a704478e1d0a78911842026d64186b
kairvee <imt_2023037@iiitm.ac.in>
2024-12-31 12:28:37 +0530

    below the image

--- src/components/imageslider.jsx
+++ src/components/imageslider.jsx
@@ -6,18 +6,18 @@ const ImageSlider = () => {
       src: "/assets/i1.jpg",
       link: "",
       caption:
-        "Terminus is donated Rs.5 lakhs to Care-O-Safe on behalf of 100 valued well-wishers. With this contribution, we aim to improve the lives of over 2,300 individuals.Care-O-Safe manufactures reusable menstrual products, making menstrual care both eco-friendly and cost-effective.",
+        "Terminus is donated Rs.5 lakhs to Care-O-Safe on behalf of 100 valued well-wishers. With this contribution, we aim to improve the lives of over 2,300 individuals. Care-O-Safe manufactures reusable menstrual products, making menstrual care both eco-friendly and cost-effective.",
     },
     {
       src: "/assets/i2.jpg",
       link: "",
-      caption: "Contributed by providing scholarships for students from economically poor backgrounds",
+      caption: "Contributed by providing scholarships for students from economically poor backgrounds",
     },
     {
       src: "/assets/i3.jpg",
       link: "#",
       caption:
-        "Long association with IMPACT, a non-profit working for children with cancer based out of Hyderabad, with INR 5L in November 2021",
+        "Long association with IMPACT, a non-profit working for children with cancer based out of Hyderabad, with INR 5L in November 2021",
     },
     {
       src: "/assets/i6.jpg",
@@ -27,30 +27,30 @@ const ImageSlider = () => {
     {
       src: "/assets/i5.jpg",
       link: "",
-      caption: "Built the Bharosa Centre for Telangana Police with an investment of INR 1Cr in August 2021",
+      caption: "Built the Bharosa Centre for Telangana Police with an investment of INR 1Cr in August 2021",
     },
     {
       src: "/assets/i4.jpg",
       link: "#",
-      caption: "Donated USD $100K for Sparsh Hospice's Palliative Care facility at Kajaguda in September 2021",
+      caption: "Donated USD $100K for Sparsh Hospice's Palliative Care facility at Kajaguda in September 2021",
     },
     {
       src: "/assets/i9.jpg",
       link: "",
       caption:
-        "CMD Mr. S.P. Reddy was felicitated by Telangana Home Minister Md. Mahmood Ali for his pivotal role and spearheading the development of the Telangana State Special Police Convention Centre",
+        "CMD Mr. S.P. Reddy was felicitated by Telangana Home Minister Md. Mahmood Ali for his pivotal role and spearheading the development of the Telangana State Special Police Convention Centre",
     },
     {
       src: "/assets/i8.jpg",
       link: "",
       caption:
-        "Adopted 13 fire stations out of 16 under the District Fire Officer Ranga Reddy Division, contributing INR 50L to Telangana Fire Services Department in 2017.",
+        "Adopted 13 fire stations out of 16 under the District Fire Officer Ranga Reddy Division, contributing INR 50L to Telangana Fire Services Department in 2017.",
     },
     {
       src: "/assets/i7.jpg",
       link: "#",
       caption:
-        " Contributed INR 1Cr to AIG Rural Outreach Program for advanced medical care in villages in August 2021. Also established a specialized Inflammatory Bowel Disease (IBD) center at AIG",
+        "Contributed INR 1Cr to AIG Rural Outreach Program for advanced medical care in villages in August 2021. Also established a specialized Inflammatory Bowel Disease (IBD) center at AIG",
     },
   ];
 
@@ -74,24 +74,25 @@ const ImageSlider = () => {
             alt={`Image ${currentIndex + 1}`}
             className="w-full transition-transform duration-100 h-[75vh] object-cover object-top"
           />
-          {/* Caption */}
-          <div className="absolute bottom-2 left-0 right-0 text-white text-sm text-center py-1 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-            {images[currentIndex].caption}
-          </div>
         </a>
+      </div>
+
+      {/* Caption Below Slider */}
+      <div className="mt-4 text-right leading-tight tracking-tighter text-black text-md">
+        {images[currentIndex].caption}
       </div>
 
       {/* Left Arrow */}
       <button onClick={prevSlide} className="absolute top-1/2 left-0 transform -translate-y-1/2 p-2">
         <svg className="w-10 h-9" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="white">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M15 19l-7-7 7-7" />
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="4" d="M15 19l-7-7 7-7" />
         </svg>
       </button>
 
       {/* Right Arrow */}
       <button onClick={nextSlide} className="absolute top-1/2 right-0 transform -translate-y-1/2 p-2">
         <svg className="w-10 h-8" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="white">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M9 5l7 7-7 7" />
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="4" d="M9 5l7 7-7 7" />
         </svg>
       </button>
     </div>
